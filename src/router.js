119675776cf24a145cb6f542f270d58b65a08ddc
Kristián Cabala <kristiancabala@gmail.com>
2019-04-23 22:15:19 +0200

    aaa

--- src/router.js
+++ src/router.js
@@ -18,7 +18,7 @@ module.exports = (config, ocr) => {
   const prodDictionary = "keywords.json";
 
   // fixing cors error on client in dev env
-  const prod = false;
+  const prod = true;
   app.use(cors({origin: prod ? "https://contractbuddy.herokuapp.com" : "http://localhost:4200"}));
   app.options('*', cors());
 
