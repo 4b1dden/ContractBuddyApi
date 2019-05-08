c6428ebfa551cfec86096da92279adc0a23d40ac
Kiko <kristiancabala@gmail.com>
2019-05-08 13:21:27 +0200

    fix

--- src/mongoWrapper.js
+++ src/mongoWrapper.js
@@ -2,7 +2,6 @@ module.exports = function(config){
   const mongoose = require('mongoose')
 
   mongoose.connect(config.mongoConnectionString, { useNewUrlParser: true })
-  mongoose.
 
   const User = require('./models/User.js')(mongoose)
 
